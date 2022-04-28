35d1d1c90081db3d3948efb013615e83a7b72e27
Adrian Kotlinski <adrian.kotlinski@rb.com>
2022-04-28 15:15:10 +0200

    Added new directives and pipelines to our smart shopping list

---
+++ src/app/shared/date.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDate]'
+})
+export class DateDirective {
+  @Input()
+  public date: Date | undefined;
+
+  private paragraph;
+
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+    this.paragraph = this.renderer.createElement('p');
+
+  }
+
+  @HostListener('mouseenter') mouseEnter(eventDate: Event) {
+    console.log("mouseenter: ", this.date);
+    this.paragraph.innerHTML = `${this.date?.toLocaleDateString()} ${this.date?.toLocaleTimeString()}`;
+    this.renderer.appendChild(this.element.nativeElement, this.paragraph);
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventDate: Event) {
+    console.log("mouseleave");
+    this.renderer.removeChild(this.element.nativeElement, this.paragraph);
+  }
+
+}
