e3348a2c98dae514b7ad59b58e3d0199bffe1527
Adrian Kotlinski <adrian.kotlinski@rb.com>
2023-01-19 21:05:36 +0100

    feature(T27): Wave and google lighthouse accessibility improvements

--- src/app/components/language-selector/language-selector.module.ts
+++ src/app/components/language-selector/language-selector.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LanguageSelectorComponent } from './language-selector.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [LanguageSelectorComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, TranslateModule],
   exports: [LanguageSelectorComponent],
 })
 export class LanguageSelectorModule {}
